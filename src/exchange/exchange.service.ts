7f71483aba778c42a72aeca9c7dc9f6ef4b6bb19
Jonny D. Lezcano <jonny@quanto.la>
2021-02-25 10:52:22 -0300

    ensure convertAmount called getCurrency twice

--- src/exchange/exchange.service.ts
+++ src/exchange/exchange.service.ts
@@ -1,10 +1,18 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 
+export class CurrenciesService {
+  async getCurrency(currency: string): Promise<any> {}
+}
+
 @Injectable()
 export class ExchangeService {
+  constructor(private currenciesServices: CurrenciesService) {}
   async convertAmount({ from, to, amount }): Promise<any> {
     if (!from || !to || !amount) {
       throw new BadRequestException();
     }
+
+    const currencyFrom = this.currenciesServices.getCurrency(from);
+    const currencyTo = this.currenciesServices.getCurrency(to);
   }
 }
