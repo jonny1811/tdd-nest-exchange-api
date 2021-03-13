512ecce1cd01ceb190e4c1fd9262d7d8035ca7d6
Jonny D. Lezcano <jonny@quanto.la>
2021-03-13 17:40:04 -0300

    create method createCurrency in currencies service

--- src/currencies/currencies.service.ts
+++ src/currencies/currencies.service.ts
@@ -1,18 +1,31 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 
+export class Currencies {
+  currency: string;
+  value: number;
+}
 export class CurrenciesRepository {
-  async getCurrency(currency: string): Promise<any> {};
+  async getCurrency(currency: string): Promise<Currencies> {
+    return new Currencies();
+  }
+
+  async createCurrency({ currency, value }): Promise<Currencies> {
+    return new Currencies();
+  }
 }
 
 @Injectable()
 export class CurrenciesService {
   constructor(private curreciesRepository: CurrenciesRepository) {}
 
-  async getCurrency(currency: string): Promise<any> {
-    try {
-      return await this.curreciesRepository.getCurrency(currency);
-    } catch (error) {
-      throw new InternalServerErrorException(error);
+  async getCurrency(currency: string): Promise<Currencies> {
+    return await this.curreciesRepository.getCurrency(currency);
+  }
+
+  async createCurrency({ currency, value }): Promise<Currencies> {
+    if (value <= 0) {
+      throw new BadRequestException('The value must be greater zero');
     }
+    return await this.curreciesRepository.createCurrency({ currency, value });
   }
 }
